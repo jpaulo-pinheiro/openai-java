77e9e2dec556d70157e4acc4ee68e160e823db72
05487496633 <joao-paulo.pinheiro@serpro.gov.br>
2024-06-25 15:18:26 -0300

    first commit

---
+++ src/main/java/br/com/travessoaoavesso/TestaIntegracao.java
@@ -0,0 +1,30 @@
+package br.com.travessoaoavesso;
+
+import java.util.Arrays;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+public class TestaIntegracao {
+
+    public static void main(String[] args) {
+
+        System.out.println("Integração Java x OpenAI\n");
+
+        var user = "gere 5 produtos";
+        var system = "voce e um gerador de produtos ficticios para um ecommerce e deve gerar apenas os nomes dos produtos solicitados";
+
+        var chave = System.getenv("OPENAI_API_KEY");
+        OpenAiService service = new OpenAiService(chave);
+        ChatCompletionRequest completionRequest = ChatCompletionRequest.builder()
+                .model("gpt-4")
+                .messages(Arrays.asList(new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)))
+                .build();
+
+        service.createChatCompletion(completionRequest).getChoices().forEach(c -> System.out.println(c.getMessage().getContent()));
+
+    }
+}
---
+++ src/test/java/br/com/travessoaoavesso/TestaIntegracaoTest.java
@@ -0,0 +1,13 @@
+package br.com.travessoaoavesso;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestaIntegracaoTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
