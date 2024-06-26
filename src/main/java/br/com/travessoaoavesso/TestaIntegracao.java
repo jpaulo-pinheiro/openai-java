8f05d945953d6bd016e57eaf973f43306be019c2
05487496633 <joao-paulo.pinheiro@serpro.gov.br>
2024-06-26 17:20:56 -0300

    Refatoração

--- src/main/java/br/com/travessoaoavesso/TestaIntegracao.java
+++ src/main/java/br/com/travessoaoavesso/TestaIntegracao.java
@@ -9,22 +9,24 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class TestaIntegracao {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        System.out.println("Integração Java x OpenAI\n");
+                System.out.println("Integração Java x OpenAI\n");
 
-        var user = "gere 5 produtos";
-        var system = "voce e um gerador de produtos ficticios para um ecommerce e deve gerar apenas os nomes dos produtos solicitados";
+                var user = "gere 5 produtos";
+                var system = "voce e um gerador de produtos ficticios para um ecommerce e deve gerar apenas os nomes dos produtos solicitados";
 
-        var chave = System.getenv("OPENAI_API_KEY");
-        OpenAiService service = new OpenAiService(chave);
-        ChatCompletionRequest completionRequest = ChatCompletionRequest.builder()
-                .model("gpt-4")
-                .messages(Arrays.asList(new ChatMessage(ChatMessageRole.USER.value(), user),
-                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)))
-                .build();
+                var chave = System.getenv("OPENAI_API_KEY");
+                OpenAiService service = new OpenAiService(chave);
+                ChatCompletionRequest completionRequest = ChatCompletionRequest.builder()
+                                .model("gpt-4")
+                                .messages(Arrays.asList(new ChatMessage(ChatMessageRole.USER.value(), user),
+                                                new ChatMessage(ChatMessageRole.SYSTEM.value(), system)))
+                                // .n(5) // numero de respostas
+                                .build();
 
-        service.createChatCompletion(completionRequest).getChoices().forEach(c -> System.out.println(c.getMessage().getContent()));
+                service.createChatCompletion(completionRequest).getChoices()
+                                .forEach(c -> System.out.println(c.getMessage().getContent()));
 
-    }
+        }
 }
